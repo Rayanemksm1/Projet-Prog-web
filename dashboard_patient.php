f4f3c0d232d799ef85c62041bdfcc2ded1993294
Rayanemksm1 <rayanemksm5@gmail.com>
2025-05-17 13:00:09 +0200

    Update dashboard_patient.php

--- dashboard_patient.php
+++ dashboard_patient.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['id']) || $_SESSION['role'] !== 'patient') {
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Tableau de bord patient</title><link rel="stylesheet" href="css/style.css"></head>
+<head><title>Tableau de bord patient</title><link rel="stylesheet" href="style.css"></head>
 <body><div class="container">
 <h2>Bienvenue, <?= $_SESSION['nom'] ?> (patient)</h2>
 <p><a href="reserver.php">Prendre un rendez-vous</a></p>
