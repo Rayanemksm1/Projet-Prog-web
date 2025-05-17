54eea9a515904eee46271e7a37647e244fbd251a
Rayanemksm1 <rayanemksm5@gmail.com>
2025-05-17 12:59:51 +0200

    Update dashboard_medecin.php

--- dashboard_medecin.php
+++ dashboard_medecin.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['id']) || $_SESSION['role'] !== 'medecin') {
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Tableau de bord médecin</title><link rel="stylesheet" href="css/style.css"></head>
+<head><title>Tableau de bord médecin</title><link rel="stylesheet" href="style.css"></head>
 <body><div class="container">
 <h2>Bienvenue, <?= $_SESSION['nom'] ?> (médecin)</h2>
 <p><a href="logout.php">Se déconnecter</a></p>
